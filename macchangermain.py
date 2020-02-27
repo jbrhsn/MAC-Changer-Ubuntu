ca7168ae75e9e6289b54f5add99796cabc523baa
jbrhsn <jabirhusainkp@yahoo.com>
2020-02-27 22:18:16 +0530

    26 FEB 10:17 PM

--- macchangermain.py
+++ macchangermain.py
@@ -2,15 +2,19 @@ import macchangerclass
 import macchangerfunction
 
 # Title And Intro
-print("\n MAC-CHANGER / MAC-SPOOFER \n  You can change the MAC Address of your network \ninterface card temporarily.\n")
+print("\n MAC-CHANGER / MAC-SPOOFER \n  You can change the MAC Address of your network "
+      "\ninterface card temporarily.\n")
 # Show Choices To User and Getting Choice Number
-print("[+] 1. Show All Interfaces \n[+] 2. Show Current MAC Address \n[+] 3. Change MAC Address \n[+] 4. Check For Dependencies \n")
-ch = input("\n Enter Your Choice : ")
+print(
+    "[+] 1. Show All Interfaces \n[+] 2. Show Current MAC Address \n"
+    "[+] 3. Change MAC Address \n[+] 4. Check For Dependencies \n")
+ch = input("\nEnter Your Choice : ")
+ch = int(ch)
 
 dumy = macchangerclass.macChanger()
 
 if ch == 1:
-    dumy.showInterfaces()
+    print("\nThe Available Interfaces Are :")
+    macchangerclass.macChanger.showInterfaces()
 if ch == 2:
     pass
-    
