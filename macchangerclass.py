eb7d3460c56bb06137b9f9c9ea3d6f29f1e7cfb3
jbrhsn <jabirhusainkp@yahoo.com>
2020-02-27 22:05:12 +0530

    Switched To Pycharm

--- macchangerclass.py
+++ macchangerclass.py
@@ -1,6 +1,42 @@
 #! /env/python3
 
 class macChanger:
-	def __init__(current):
+	def __init__(interface):
+		self.interface=interface
 		self.current_mac=current
+
+	def __init__():
+		pass
+
+	def showInterfaces():
+		# Fetching all interfaces
+		out = subprocess.check_output(["ifconfig"],shell=True)
+		interfaces = re.findall(r"(\w+)(?:: flags)",str(out))
+		# Display all the interfaces
+		i=1
+		for interface in interfaces:
+			print("\n[+] "+str(i)+" : "+interface)
+			i=i+1
+		# Returns the list of interfaces
+		return interfaces
+
+	def askMac():
+		#   Get Mac Address From User
+		mac = input("\n[+] Enter New Mac Address : ")
+		new_mac = re.findall(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w",mac)
+		# Checks if it is entered in correct form
+		if(new_mac[0] == mac):
+			return new_mac
+		else:
+			print("\n[-] Not a Valid MAC Address. Eg: a1:b2:c3:4a:66:bs ")
+			askMac()
+
+	def showCurrentMac(interface):
+		# Fetching MAC Address
+		out = subprocess.check_output(["ifconfig",interface],shell=True)
+		current_mac = re.findall(r"(\w\w:\w\w:\w\w:\w\w:\w\w)",str(out))
+		# Display Current MAC Address
+		print("\n[+] Current MAC Address : "+current_mac[0])
+		# Returns Current MAC Address
+		return current_mac
 
--- macchangermain.py
+++ macchangermain.py
@@ -1,6 +1,16 @@
 import macchangerclass
-import macchangerfunctions
+import macchangerfunction
 
-print("\n MAC-CHANGER / MAC-SPOOFER \n")
+# Title And Intro
+print("\n MAC-CHANGER / MAC-SPOOFER \n  You can change the MAC Address of your network \ninterface card temporarily.\n")
+# Show Choices To User and Getting Choice Number
 print("[+] 1. Show All Interfaces \n[+] 2. Show Current MAC Address \n[+] 3. Change MAC Address \n[+] 4. Check For Dependencies \n")
 ch = input("\n Enter Your Choice : ")
+
+dumy = macchangerclass.macChanger()
+
+if ch == 1:
+    dumy.showInterfaces()
+if ch == 2:
+    pass
+    
