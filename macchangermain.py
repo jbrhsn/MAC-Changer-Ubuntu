0e8c93eae1e1702f2309479ab2706a6c91e3bcfa
jbrhsn <jabirhusainkp@yahoo.com>
2020-02-27 21:35:55 +0530

    3 Functions Defined

---
+++ macchangermain.py
@@ -0,0 +1,6 @@
+import macchangerclass
+import macchangerfunctions
+
+print("\n MAC-CHANGER / MAC-SPOOFER \n")
+print("[+] 1. Show All Interfaces \n[+] 2. Show Current MAC Address \n[+] 3. Change MAC Address \n[+] 4. Check For Dependencies \n")
+ch = input("\n Enter Your Choice : ")
