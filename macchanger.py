3c8a6f88bf94f9a34b49ee89ef885d66ed438853
jbrhsn <jabirhusainkp@yahoo.com>
2020-03-17 22:16:42 +0530

    "Automated Commit : 2020-03-17"

--- macchanger.py
+++ macchanger.py
@@ -2,6 +2,7 @@ import sys
 from files import macchangerfunctions
 
 
+# Checking for Commandline Inputs
 def getCommandLineInputs():
     if len(sys.argv) > 1:
         macchangerfunctions.start('command', sys.argv)
